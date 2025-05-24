87808222b4daea367beb52c15e6b687128ff55f1
Ali Hamzehpour <alihamzehpour2002@gmail.com>
2025-05-24 15:05:11 +0330

    complete step 1

---
+++ Project-2/6.5840/src/kvsrv1/server.go
@@ -0,0 +1,84 @@
+package kvsrv
+
+import (
+	"log"
+	"sync"
+
+	"6.5840/kvsrv1/rpc"
+	"6.5840/labrpc"
+	tester "6.5840/tester1"
+)
+
+const Debug = false
+
+func DPrintf(format string, a ...interface{}) (n int, err error) {
+	if Debug {
+		log.Printf(format, a...)
+	}
+	return
+}
+
+type ValueVersionPair struct {
+	Value   string
+	Version rpc.Tversion
+}
+
+type KVServer struct {
+	mu sync.Mutex
+	mappings map[string]ValueVersionPair
+}
+
+func MakeKVServer() *KVServer {
+	kv := &KVServer{}
+	kv.mappings = make(map[string]ValueVersionPair)
+	return kv
+}
+
+// Get returns the value and version for args.Key, if args.Key
+// exists. Otherwise, Get returns ErrNoKey.
+func (kv *KVServer) Get(args *rpc.GetArgs, reply *rpc.GetReply) {
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+	if pair, exists := kv.mappings[args.Key]; exists {
+		reply.Value = pair.Value
+		reply.Version = pair.Version
+		reply.Err = rpc.OK
+	} else {
+		reply.Err = rpc.ErrNoKey
+	}
+}
+
+// Update the value for a key if args.Version matches the version of
+// the key on the server. If versions don't match, return ErrVersion.
+// If the key doesn't exist, Put installs the value if the
+// args.Version is 0, and returns ErrNoKey otherwise.
+func (kv *KVServer) Put(args *rpc.PutArgs, reply *rpc.PutReply) {
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+	if pair, exists := kv.mappings[args.Key]; exists {
+		if pair.Version == args.Version {
+			kv.mappings[args.Key] = ValueVersionPair{Value: args.Value, Version: args.Version + 1}
+			reply.Err = rpc.OK
+		} else {
+			reply.Err = rpc.ErrVersion
+		}
+	} else {
+		if args.Version == 0 {
+			kv.mappings[args.Key] = ValueVersionPair{Value: args.Value, Version: 1}
+			reply.Err = rpc.OK
+		} else {
+			reply.Err = rpc.ErrNoKey
+		}
+	}
+}
+
+// You can ignore Kill() for this lab
+func (kv *KVServer) Kill() {
+}
+
+
+// You can ignore all arguments; they are for replicated KVservers
+func StartKVServer(ends []*labrpc.ClientEnd, gid tester.Tgid, srv int, persister *tester.Persister) []tester.IService {
+	kv := MakeKVServer()
+	return []tester.IService{kv}
+}
